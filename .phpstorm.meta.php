7fa78a2d29604485502221f137b61bd5ef3f7282
AlexCatch <alex@alexcatchpoledev.me>
2018-07-22 21:50:30 +0100

    Add Dusk for browser tests

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-07-22 18:52:26.
+    * Generated on 2018-07-22 20:35:45.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
