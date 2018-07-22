8c800fba74f4c07fefa6a0c375a8a365ebd5d90e
AlexCatch <alex@alexcatchpoledev.me>
2018-07-22 20:38:24 +0100

    ESLint, React, SCSS, Ziggy, JWTAuth, LogViewe

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-laravel-boilerplate
+# Laravel Boilerplate
+
+This is our internal boilerplate for Laravel projects, it should hopefully reduce the time it takes for you to setup a project & also improve code standards across the board.
+
+The table below illustrates the planned feature and the currently implemented features.
+
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
---
+++ resources/assets/js/components/Example.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Example = () => (
+  <div className="container">
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-header">
+            Example Component
+          </div>
+          <div className="card-body">
+            I&quot;m an example component!
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+if (document.getElementById('example')) {
+  ReactDOM.render(<Example />, document.getElementById('example'));
+}
