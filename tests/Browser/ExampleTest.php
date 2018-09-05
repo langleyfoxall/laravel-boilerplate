b41539391591b29014d1293bf4cfc2f0f8620d92
Jordan Hall <jordan@hall05.co.uk>
2018-09-05 11:45:41 +0100

    Fix tests

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -17,8 +17,7 @@ class ExampleTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                    ->dump();
-//                    ->assertSee('Laravel');
+                    ->assertSee('Laravel');
         });
     }
 }
