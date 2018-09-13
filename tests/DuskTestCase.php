ee916e2ae774915ccc48b096875aadf0c2cd005b
Laravel Shift <shift@laravelshift.com>
2018-09-13 17:13:40 +0000

    Adopt PSR-2 coding style
    The Laravel framework adopts the PSR-2 coding style in version 5.1.
    Laravel apps *should* adopt this coding style as well. Read the
    [PSR-2 coding style guide][1] for more details and check out [PHPCS][2]
    to use as a code formatting tool.
    
    [1]: https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-2-coding-style-guide.md
    [2]: https://github.com/squizlabs/PHP_CodeSniffer

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -52,8 +52,10 @@ abstract class DuskTestCase extends BaseTestCase
         ]);
 
         $driver = RemoteWebDriver::create(
-            'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
-                ChromeOptions::CAPABILITY, $options
+            'http://localhost:9515',
+            DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY,
+                $options
             )
         );
       
