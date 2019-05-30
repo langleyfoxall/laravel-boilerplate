cc746206c376b6b25635fa42639a25967887da7b
Ashley <ashleyd223@gmail.com>
2019-05-30 12:52:20 +0100

    Update editorconfig to use 4 spaces. Add debug blacklist to config.

--- config/app.php
+++ config/app.php
@@ -213,4 +213,45 @@ return [
 
     ],
 
+    /*
+     |--------------------------------------------------------------------------
+     | Debug Blacklist
+     |--------------------------------------------------------------------------
+     |
+     | Hide environment variable values from the exception page.
+     |
+     */
+
+    'debug_blacklist' => [
+        '_ENV' => [
+            'APP_KEY',
+            'DB_DATABASE',
+            'DB_USERNAME',
+            'DB_PASSWORD',
+            'REDIS_PASSWORD',
+            'REDIS_PORT',
+            'MAIL_USERNAME',
+            'MAIL_PASSWORD',
+            'LOGS_USERNAME',
+            'LOGS_PASSWORD',
+        ],
+
+        '_SERVER' => [
+            'APP_KEY',
+            'DB_DATABASE',
+            'DB_USERNAME',
+            'DB_PASSWORD',
+            'REDIS_PASSWORD',
+            'REDIS_PORT',
+            'MAIL_USERNAME',
+            'MAIL_PASSWORD',
+            'LOGS_USERNAME',
+            'LOGS_PASSWORD',
+        ],
+
+        '_POST' => [
+            'password',
+        ],
+    ],
+
 ];
